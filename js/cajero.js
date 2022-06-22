c6bf6daad31d8ce1d8194e6ba3f69e3de7c1ccf9
Alejandro Angeles <alejandro.angeles9838@gmail.com>
2022-06-22 11:10:24 -0500

    Validacion de retiro y deposito

--- js/cajero.js
+++ js/cajero.js
@@ -6,11 +6,30 @@ var cuentas = [
 
 function saldo(){
   const usuarioLogueado = localStorage.getItem("cuentaLogueada");
-  console.log(usuarioLogueado);
+  
   for (let i = 0; i < cuentas.length; i++) {
     if(usuarioLogueado == cuentas[i].nombre){
-      console.log(cuentas[i].saldo)
+      alert(usuarioLogueado + " cuentas con un saldo de " + "$" + cuentas[i].saldo)
     }
   }
 }
 
+function retirarEfectivo() {
+  const usuarioLogueado = localStorage.getItem("cuentaLogueada");
+  let cantidadRetirar = parseInt(prompt('Ingresa la cantidad que desea retirar'))
+  for (let i = 0; i < cuentas.length; i++) {
+   if(usuarioLogueado == cuentas[i].nombre){
+      alert( "Ahora cuentas con un saldo de "+ "$" + (cuentas[i].saldo - cantidadRetirar) );
+    } 
+}
+}
+
+function ingresarEfectivo() {
+  const usuarioLogueado = localStorage.getItem("cuentaLogueada");
+  let cantidadIngresar = parseInt(prompt('Ingresa la cantidad que desea depositar'))
+  for (let i = 0; i < cuentas.length; i++) {
+    if(usuarioLogueado == cuentas[i].nombre){
+      alert( "Ahora cuentas con un saldo de "+ "$" + (cuentas[i].saldo + cantidadIngresar) );
+    }
+  }
+}
